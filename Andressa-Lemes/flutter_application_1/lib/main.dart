524c906c531dfc8d8b9cfc6095f0d05d8e67e629
Andrade <andressaandrade461@gmail.com>
2022-03-04 21:23:41 -0400

    a

--- Andressa-Lemes/flutter_application_1/lib/main.dart
+++ Andressa-Lemes/flutter_application_1/lib/main.dart
@@ -1,4 +1,31 @@
-import 'dart:html';
+
+
+// ignore_for_file: deprecated_member_use
 
 import 'package:flutter/material.dart';
+
+ main ()=>runApp(const JardimdoApp());
+  class JardimdoApp extends StatelessWidget {
+  const JardimdoApp({Key? key}) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Jardim do Céu!!!'),
+          ),
+          body: Column(
+            children: <Widget>[
+             Text('Jardim do C'[0]),
+             const RaisedButton(
+               child: Text('JardimdoApp 1'),
+               onPressed: null,
+             )
+            ],
+        ),
+      ),
+    };
+  }
+
 
