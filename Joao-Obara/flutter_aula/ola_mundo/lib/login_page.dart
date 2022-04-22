308003c366c02452811215c69cfbeea477ed1338
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-04-22 18:42:30 -0400

    fim aula 1.12.3

--- Joao-Obara/flutter_aula/ola_mundo/lib/login_page.dart
+++ Joao-Obara/flutter_aula/ola_mundo/lib/login_page.dart
@@ -8,42 +8,55 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String usuario = "";
+  String senha = "";
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: SizedBox(
-        height: double.infinity,
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              TextField(
-                keyboardType: TextInputType.emailAddress, //tipo de teclado
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  border: OutlineInputBorder(),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (text) => usuario =
+                      text, //quando tiver mudança passar o email para a variavel usuario
+                  keyboardType: TextInputType.emailAddress, //tipo de teclado
+                  decoration: const InputDecoration(
+                    labelText: 'E-mail',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 25,
-              ),
-              TextField(
-                obscureText: true, //Não mostrar a senha
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  border: OutlineInputBorder(),
+                const SizedBox(
+                  height: 25,
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                onPressed: () {}, //Corrigir
-                child: Text('Login'),
-              )
-            ],
+                TextField(
+                  onChanged: (text) => senha = text,
+                  obscureText: true, //Não mostrar a senha
+                  decoration: const InputDecoration(
+                    labelText: 'Senha',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    //Login simulado
+                    if (usuario == "root" && senha == "root") {
+                      print("Login correto");
+                    } else
+                      print("Login errado");
+                  }, //Corrigir
+                  child: const Text('Login'),
+                )
+              ],
+            ),
           ),
         ),
       ),
