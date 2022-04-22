1860bfc2f08096193595d0b87266243c6d6fee76
Joao Ricardo Obara <joao.ricardo.obara@gmail.com>
2022-04-21 21:14:18 -0400

    final aula 1.7.2

--- Joao-Obara/flutter_aula/ola_mundo/lib/app_widget.dart
+++ Joao-Obara/flutter_aula/ola_mundo/lib/app_widget.dart
@@ -3,14 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:ola_mundo/home_page.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key, required this.title}) : super(key: key);
-
   final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.red),
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        brightness: Brightness.dark,
+      ),
       home: const HomePage(),
     );
   }
