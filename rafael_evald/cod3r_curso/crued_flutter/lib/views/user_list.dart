cac7056bcddd4e59680d81d049ef9079fd76e346
Rafael Evald Silva <rafael-ev-silva@outlook.com>
2022-04-24 11:14:30 -0400

    Atividade crued

---
+++ rafael_evald/cod3r_curso/crued_flutter/lib/views/user_list.dart
@@ -0,0 +1,41 @@
+import 'package:crued_flutter/components/user_tile.dart';
+import 'package:crued_flutter/models/user.dart';
+import 'package:crued_flutter/provider/users.dart';
+import 'package:crued_flutter/routes/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserList extends StatelessWidget {
+  const UserList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Users users = Provider.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Usuários'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.userForm,
+                arguments: const User(
+                  id: '',
+                  name: '',
+                  email: '',
+                  avatarUrl: '',
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
+      ),
+    );
+  }
+}
