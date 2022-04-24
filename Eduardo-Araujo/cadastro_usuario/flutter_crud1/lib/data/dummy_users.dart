6f1fe14e1724c234c109a36d218111643cbc3597
Eduardo de Araújo Santos <dudueduardo11@gmail.com>
2022-04-24 03:35:58 -0400

    Curso flutter - Cadastro_usuário - cod3

---
+++ Eduardo-Araujo/cadastro_usuario/flutter_crud1/lib/data/dummy_users.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_crud/models/user.dart';
+
+const dummyUsers = {
+  '1': User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@yahoo.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2020/04/15/17/32/medical-5047582_1280.png',
+  ),
+  '2': User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafael@alunos.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png',
+  ),
+  '3': User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_1280.png',
+  ),
+  '4': User(
+    id: '4',
+    name: 'Guilherme',
+    email: 'gui@hotmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_1280.png',
+  ),
+  '5': User(
+    id: '5',
+    name: 'Lana',
+    email: 'lana@outlook.com',
+    avatarUrl: '',
+  )
+};
